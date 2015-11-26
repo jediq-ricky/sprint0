6e578adbdf09d4a3e987b0a50bdacf7e8804830d
Ricky Walker <ricky.walker@kainos.com>
2015-11-26 18:29:33 +0000

    Added puppetVars so we can dynamically create the tool links in the dashboard

---
+++ builder/nginx/dashboard/puppet-vars.js
@@ -0,0 +1,24 @@
+var puppetVars = {
+    tools: [
+        {
+            name: 'jenkins',
+            image: 'img/jenkins.png',
+            url: 'http://192.168.99.100:8000'
+        },
+        {
+            name: 'artifactory',
+            image: 'img/artifactory.png',
+            url: 'http://192.168.99.100:8010'
+        },
+        {
+            name: 'gitlab',
+            image: 'img/gitlab.svg',
+            url: 'http://192.168.99.100:8020'
+        },
+        {
+            name: 'sonarqube',
+            image: 'img/sonarqube.png',
+            url: 'http://192.168.99.100:8030'
+        }
+    ]
+}
